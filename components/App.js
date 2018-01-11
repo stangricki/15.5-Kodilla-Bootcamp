60f22453036bba316fe21b4c9e8eaf68ce3e53d6
Filip Stangricki <stangricki@gmail.com>
2018-01-11 23:06:51 +0100

    First commit

---
+++ components/App.js
@@ -0,0 +1,77 @@
+var GIPHY_API_URL = 'https://api.giphy.com',
+GIPHY_PUB_KEY = '9af6d92a7b0d48a4812c800105edda24';
+
+const url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText; 
+
+App = React.createClass({
+
+    getInitialState() {
+    return {
+        loading: false,
+        searchingText: '',
+        gif: {}
+    };
+},
+
+
+getGif: function(searchingText, callback) {
+    return new Promise(
+        function(resolve, reject) {
+            const xhr = new XMLHttpRequest(); 
+            xhr.onload = function() {
+                if (xhr.status === 200) {
+                    resolve(this.resolve);
+                    var data = JSON.parse(xhr.responseText).data; 
+                    var gif = { 
+                        url: data.fixed_width_downsampled_url,
+                        sourceUrl: data.url
+                    };
+                    callback(gif); 
+                } else {reject(new Error(this.statusText));}
+            };
+            xhr.open('GET', url);
+            xhr.send();
+        });
+    );
+    getGif(url)
+    .then(response => console.log('Contents: ' + response))
+    .catch(error => console.error('Something went wrong', error));
+},
+
+handleSearch: function(searchingText) { 
+    this.setState({
+      loading: true 
+    });
+    this.getGif(searchingText, function(gif) { 
+      this.setState({ 
+        loading: false,  
+        gif: gif,  
+        searchingText: searchingText 
+      });
+    }.bind(this));
+  },
+
+
+
+    render: function() {
+            
+        var styles = {
+            margin: '0 auto',
+            textAlign: 'center',
+            width: '90%'
+        };
+        
+        return (
+          <div style={styles}>
+                <h1>Wyszukiwarka GIFow!</h1>
+                <p>Znajdź gifa na <a href='http://giphy.com'>giphy</a>. Naciskaj enter, aby pobrać kolejne gify.</p>
+                <Search onSearch={this.handleSearch}/>
+            <Gif
+    loading={this.state.loading}
+    url={this.state.gif.url}
+    sourceUrl={this.state.gif.sourceUrl}
+/>
+          </div>
+        );
+    }
+});
